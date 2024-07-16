25f8829b2967cd8fbe56070955e7f636dc6db07d
pedrodecf <pedro012014@gmail.com>
2024-07-16 18:30:01 -0300

    feat: :lipstick: adicionado animações com framer-motion

--- src/app/_components/CardBox.tsx
+++ src/app/_components/CardBox.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
 import { DownloadIcon, CaretRightIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
+import { FadeIn } from './animations/fade-in'
 
 interface CardProps {
   className?: string
@@ -25,75 +26,85 @@ export function CardBox(props: CardProps) {
   const hasActivity = props.activity && props.activity.length > 0
 
   return (
-    <Card className={`${props.className}`}>
-      <CardHeader className="flex flex-row align-middle justify-between">
-        <h2 className="text-xl font-bold flex items-center gap-2">
-          {props.title}
-        </h2>
-        {props.link && (
-          <Link
-            className="text-xs flex align-middle text-primary underline"
-            href={props.link}
-          >
-            {props.linkText}
-            <CaretRightIcon />
-          </Link>
-        )}
-      </CardHeader>
-
-      {props.description && (
-        <CardContent>
-          <p className="text-foreground font-medium opacity-60">
-            {props.description}
-          </p>
-        </CardContent>
-      )}
-
-      {hasActivity && (
-        <CardContent>
-          {props.activity!.map((item, index) => (
-            <div
-              key={index}
-              className={`flex items-center gap-3 ${
-                props.activity!.length > 1 && index < props.activity!.length - 1
-                  ? 'mb-4'
-                  : ''
-              }`}
+    <FadeIn
+      as="div"
+      className={`${props.className}`}
+      delay={0.1}
+      duration={0.4}
+    >
+      <Card>
+        <CardHeader className="flex flex-row align-middle justify-between">
+          <FadeIn delay={0.1} duration={0.4}>
+            <h2 className="text-xl font-bold flex items-center gap-2">
+              {props.title}
+            </h2>
+          </FadeIn>
+          {props.link && (
+            <Link
+              className="text-xs flex align-middle text-primary underline"
+              href={props.link}
             >
-              <Avatar>
-                <AvatarImage
-                  src={item.avatar}
-                  alt={item.title}
-                  width={48}
-                  height={48}
-                  className="rounded-full p-1 border-solid border border-border shadow-custom-light"
-                />
-                <AvatarFallback className="rounded-full p-1 w-12 h-12 border-solid border border-border shadow-custom-light" />
-              </Avatar>
-              <div>
-                <h3 className="text-sm font-semibold">{item.title}</h3>
-                <p className="text-xs font-medium opacity-60">
-                  {item.description}
-                </p>
-                <p className="text-xs font-medium opacity-60">
-                  {item.duration}
-                </p>
-              </div>
-            </div>
-          ))}
-        </CardContent>
-      )}
+              {props.linkText}
+              <CaretRightIcon />
+            </Link>
+          )}
+        </CardHeader>
 
-      {props.buttonText && (
-        <CardFooter>
-          <Button className="w-full bg-gradient-to-l from-">
-            {props.buttonIcon === 'download' && (
-              <DownloadIcon className="mr-2 h-4 w-4" />
-            )}
-            {props.buttonText}
-          </Button>
-        </CardFooter>
-      )}
-    </Card>
+        {props.description && (
+          <CardContent>
+            <FadeIn delay={0.2} duration={0.4}>
+              <p className="text-foreground font-medium opacity-60">
+                {props.description}
+              </p>
+            </FadeIn>
+          </CardContent>
+        )}
+
+        {hasActivity && (
+          <CardContent>
+            {props.activity!.map((item, index) => (
+              <FadeIn
+                as="div"
+                key={index}
+                className={`flex items-center gap-3 ${index < props.activity!.length - 1 ? 'mb-4' : ''}`}
+                delay={parseFloat(`0.${index + 2}`)}
+                duration={0.4}
+              >
+                <Avatar>
+                  <AvatarImage
+                    src={item.avatar}
+                    alt={item.title}
+                    width={48}
+                    height={48}
+                    className="rounded-full p-1 border-solid border border-border shadow-custom-light"
+                  />
+                  <AvatarFallback className="rounded-full p-1 w-12 h-12 border-solid border border-border shadow-custom-light" />
+                </Avatar>
+                <div>
+                  <h3 className="text-sm font-semibold">{item.title}</h3>
+                  <p className="text-xs font-medium opacity-60">
+                    {item.description}
+                  </p>
+                  <p className="text-xs font-medium opacity-60">
+                    {item.duration}
+                  </p>
+                </div>
+              </FadeIn>
+            ))}
+          </CardContent>
+        )}
+
+        {props.buttonText && (
+          <CardFooter>
+            <Button className="w-full bg-gradient-to-l from-">
+              {props.buttonIcon === 'download' && (
+                <DownloadIcon className="mr-2 h-4 w-4" />
+              )}
+              {props.buttonText}
+            </Button>
+          </CardFooter>
+        )}
+      </Card>
+    </FadeIn>
   )
 }
--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,55 +1,66 @@
 import Image from 'next/image'
 import { Badge } from '@/components/ui/badge'
+import { FadeIn } from './animations/fade-in'
 
 export function Header() {
   return (
     <section className="flex gap-6">
-      <Image
-        src="/others/profile-pic.jpeg"
-        alt="Logo"
-        width={100}
-        height={100}
-        priority={true}
-        className="rounded-lg border-solid border border-border shadow-custom-light"
-      />
+      <FadeIn delay={0.1} duration={0.4}>
+        <Image
+          src="/others/profile-pic.jpeg"
+          alt="Logo"
+          width={100}
+          height={100}
+          priority={true}
+          className="rounded-lg border-solid border border-border shadow-custom-light"
+        />
+      </FadeIn>
       <div className="flex flex-col align-middle justify-center">
         <div className="flex items-center">
-          <h1 className="font-extrabold text-3xl">Pedro de Freitas</h1>{' '}
-          <Badge
-            variant="outline"
-            className="ml-2 rounded-full p-1 shadow-custom-light hover:bg-primary"
-          >
-            <Image
-              src="/emojis/memoji1.png"
-              alt="memoji"
-              width={25}
-              height={25}
-            />
-          </Badge>
+          <FadeIn delay={0.1} duration={0.4}>
+            <h1 className="font-extrabold text-3xl">Pedro de Freitas</h1>
+          </FadeIn>
+          <FadeIn delay={0.1} duration={0.4}>
+            <Badge
+              variant="outline"
+              className="ml-2 rounded-full p-1 shadow-custom-light hover:bg-primary"
+            >
+              <Image
+                src="/emojis/memoji1.png"
+                alt="memoji"
+                width={25}
+                height={25}
+              />
+            </Badge>
+          </FadeIn>
         </div>
-        <p className="text-foreground font-medium opacity-60">
-          Desenvolvedor Full Stack
-        </p>
-        <div className="flex gap-3 mt-2">
-          <Badge variant="default" className="select-none">
-            React
-          </Badge>
-          <Badge variant="default" className="select-none">
-            Node.js
-          </Badge>
-          <Badge variant="default" className="select-none">
-            Next.js
-          </Badge>
-          <Badge variant="default" className="select-none">
-            NestJS
-          </Badge>
-          <Badge variant="default" className="select-none">
-            JavaScript
-          </Badge>
-          <Badge variant="default" className="select-none">
-            TypeScript
-          </Badge>
-        </div>
+        <FadeIn delay={0.1} duration={0.4}>
+          <p className="text-foreground font-medium opacity-60">
+            Desenvolvedor Full Stack
+          </p>
+        </FadeIn>
+        <FadeIn delay={0.2} duration={0.4}>
+          <div className="flex gap-3 mt-2">
+            <Badge variant="default" className="select-none">
+              React
+            </Badge>
+            <Badge variant="default" className="select-none">
+              Node.js
+            </Badge>
+            <Badge variant="default" className="select-none">
+              Next.js
+            </Badge>
+            <Badge variant="default" className="select-none">
+              NestJS
+            </Badge>
+            <Badge variant="default" className="select-none">
+              JavaScript
+            </Badge>
+            <Badge variant="default" className="select-none">
+              TypeScript
+            </Badge>
+          </div>
+        </FadeIn>
       </div>
     </section>
   )

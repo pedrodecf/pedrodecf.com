75c1c61d79606ece3bf09f07061c682d8591975c
pedrodecf <pedro012014@gmail.com>
2024-07-17 02:33:30 -0300

    feat: :sparkles: adicionando portfolio

--- src/app/_components/MiniPorfolioCard.tsx
+++ src/app/_components/MiniPorfolioCard.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { DialogTitle } from '@radix-ui/react-dialog'
 import { ArrowTopRightIcon, GitHubLogoIcon } from '@radix-ui/react-icons'
-import Image from 'next/image'
 import Link from 'next/link'
 import {
   Card,
@@ -10,11 +8,6 @@ import {
   CardFooter,
   CardHeader,
 } from '../../components/ui/card'
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from '../../components/ui/dialog'
 import { Button } from '../../components/ui/button'
 import { FadeIn } from './animations/FadeIn'
 
@@ -22,48 +15,21 @@ interface PortfolioCardProps {
   project: {
     title: string
     description: string
-    image: string
     projectLink?: string
     githubLink?: string
   }
 }
 
-export function PortfolioCard({
-  project: { title, description, image, projectLink, githubLink },
+export function MiniPortfolioCard({
+  project: { title, description, projectLink, githubLink },
 }: PortfolioCardProps) {
   return (
     <FadeIn>
       <Card className="w-full">
-        <Dialog>
-          <CardHeader>
-            <DialogTrigger>
-              <Image
-                src={image}
-                alt={title}
-                width={640}
-                height={480}
-                className="cursor-pointer rounded-lg brightness-50 duration-500 hover:scale-105 hover:brightness-100 "
-                priority={true}
-              />
-            </DialogTrigger>
-            <DialogContent
-              aria-describedby={undefined}
-              className="max-w-[65vw]"
-            >
-              <DialogTitle className="sr-only">{title}</DialogTitle>
-              <Image
-                src={image}
-                alt={title}
-                quality={100}
-                width={1280}
-                height={720}
-                priority={true}
-              />
-            </DialogContent>
-          </CardHeader>
-        </Dialog>
-        <CardContent>
+        <CardHeader className="-mb-5">
           <h2 className="font-bold text-lg">{title}</h2>
+        </CardHeader>
+        <CardContent>
           <p className="line-clamp-3">{description}</p>
         </CardContent>
         <CardFooter className="flex items-center gap-2">
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,29 +1,99 @@
+import { FadeIn } from '../_components/animations/FadeIn'
+import { MiniPortfolioCard } from '../_components/MiniPorfolioCard'
 import { PortfolioCard } from '../_components/PorfolioCard'
 
 export default function Portfolio() {
+  const projects = [
+    {
+      image: '/portfolio/screenshot-2.png',
+      title: 'Sofftek AI Chat',
+      description:
+        'Um chat alimentado por IA com o propósito de ajudar os profissionais de Service Desk do time da Softtek. Desenvolvido para o FIAP Challenge 2024.',
+      projectLink: 'https://youtu.be/ruH5agMbmcA',
+    },
+    {
+      image: '/portfolio/screenshot-4.jpg',
+      title: 'Orange Portfólio (Front-end)',
+      description:
+        'Desenvolvido durante o hackathon realizado pela FCamara, em uma squad de 4 membros, utiliza React, TypeScript, Material UI e Styled-Components.',
+      projectLink: 'https://orange-front-end.onrender.com',
+      githubLink: 'https://github.com/MatheusSanchez/orange-front',
+    },
+    {
+      image: '/portfolio/screenshot-3.jpg',
+      title: 'Orange Portfólio (Back-end)',
+      description:
+        'Desenvolvido durante o hackathon realizado pela FCamara, em uma squad de 4 membros, utiliza Node.js, AWS, Docker, Postgres e Prisma.',
+      githubLink: 'https://github.com/MatheusSanchez/orange-back',
+    },
+    {
+      image: '/portfolio/screenshot-1.jpg',
+      title: 'AnimesOneHD',
+      description:
+        'Criado em 2018, utilizava PHP e MySQL. Alcançou em seu auge quase 40 milhões de acessos mensais. Permanentemente fora do ar.',
+      projectLink: '/about',
+    },
+  ]
+
+  const otherProjects = [
+    {
+      title: 'Gympass (Back-end)',
+      description:
+        'Api desenvolvida para simular um sistema de check-in de academias, utilizando Node.js, Docker, Fastify, Zod e PostgreSQL.',
+      githubLink: 'https://github.com/pedrodecf/gympass-api',
+    },
+    {
+      title: 'Find-a-friend (Back-end)',
+      description:
+        'Api desenvolvida para um sistema de adoção de animais, utilizando Node.js, Docker, Fastify, Zod e PostgreSQL.',
+      githubLink: 'https://github.com/pedrodecf/find-a-friend-api',
+    },
+    {
+      title: 'Woovi Challenge (Front-end)',
+      description:
+        'Desafio para front-end, tela mobile estática usando React, JavaScript e Material UI. Simulando um sistema de pagamentos',
+      projectLink: 'https://woovi-frontend-enginner-challenge.vercel.app/',
+      githubLink:
+        'https://github.com/pedrodecf/woovi-frontend-enginner-challenge',
+    },
+    {
+      title: 'Ambulnzllc Challenge (Back-end)',
+      description:
+        'Desafio para back-end, api simples desenvolvida para um sistema de pizzaria, utilizando Node.js, Docker, Fastify, Zod e MySQL.',
+      githubLink: 'https://github.com/pedrodecf/ambulnzllc-fullstack-challenge',
+    },
+    {
+      title: 'Buildbox Challenge (Front-end)',
+      description:
+        'Desafio para front-end, tela de upload simples desenvolvida com TypeScript, React e Styled-Components.',
+      projectLink: 'https://buildboxwebchallenge-pedrodecf.netlify.app/',
+      githubLink:
+        'https://github.com/pedrodecf/buildbox-web-developer-challenge',
+    },
+    {
+      title: 'Tcholas Odyssey The Game',
+      description:
+        'Um jogo feito para o curso da DIO e iFood, utiliza apenas HTML, CSS e JavaScript. Fiz toda a manipulação de imagens pelo PhotoShop',
+      projectLink: 'https://pedrodecf.github.io/tcholas-odyssey-the-game/',
+      githubLink: 'https://github.com/pedrodecf/tcholas-odyssey-the-game',
+    },
+  ]
+
   return (
     <main className="flex flex-col justify-center mt-8 gap-5">
       <h2 className="text-2xl font-bold">Portfolio: </h2>
       <div className="grid grid-cols-2 gap-5">
-        <PortfolioCard
-          image="/screenshot-1.png"
-          title="AnimesOneHD"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum laudantium voluptatibus maiores assumenda, ad, reiciendis numquam impedit quibusdam"
-          projectLink="https://animesonehd.vercel.app/"
-        />
-        <PortfolioCard
-          image="/screenshot-1.png"
-          title="AnimesOneHD"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum laudantium voluptatibus maiores assumenda, ad, reiciendis numquam impedit quibusdam"
-          projectLink="https://animesonehd.vercel.app/"
-          githubLink="asdfasf"
-        />
-        <PortfolioCard
-          image="/screenshot-1.png"
-          title="AnimesOneHD"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum laudantium voluptatibus maiores assumenda, ad, reiciendis numquam impedit quibusdam"
-          projectLink="https://animesonehd.vercel.app/"
-        />
+        {projects.map((project, index) => (
+          <PortfolioCard key={index} project={project} />
+        ))}
+      </div>
+      <FadeIn as="h2" className="text-2xl font-bold mt-8">
+        Projetos menores:
+      </FadeIn>
+      <div className="grid grid-cols-2 gap-5">
+        {otherProjects.map((project, index) => (
+          <MiniPortfolioCard key={index} project={project} />
+        ))}
       </div>
     </main>
   )
